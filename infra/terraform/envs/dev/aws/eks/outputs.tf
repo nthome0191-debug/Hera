4e9fd282261ab576b80b6040d4020df33feb5ff5
natali-aharoni <natali.aharoni.computer.science@gmail.com>
2025-12-04 09:23:58 +0200

    eks module organized

--- infra/terraform/envs/dev/aws/eks/outputs.tf
+++ infra/terraform/envs/dev/aws/eks/outputs.tf
@@ -1,85 +1,68 @@
 output "cluster_id" {
-  description = "EKS cluster ID"
-  value       = module.eks_cluster.cluster_id
+  value = module.eks_cluster.cluster_id
 }
 
 output "cluster_name" {
-  description = "EKS cluster name (with random suffix if enabled)"
-  value       = module.eks_cluster.cluster_name
+  value = module.eks_cluster.cluster_name
 }
 
 output "cluster_arn" {
-  description = "EKS cluster ARN"
-  value       = module.eks_cluster.cluster_arn
+  value = module.eks_cluster.cluster_arn
 }
 
 output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
-  value       = module.eks_cluster.cluster_endpoint
+  value = module.eks_cluster.cluster_endpoint
 }
 
 output "cluster_version" {
-  description = "EKS cluster Kubernetes version"
-  value       = module.eks_cluster.cluster_version
+  value = module.eks_cluster.cluster_version
 }
 
 output "cluster_security_group_id" {
-  description = "Security group ID attached to the EKS cluster"
-  value       = module.eks_cluster.cluster_security_group_id
+  value = module.eks_cluster.cluster_security_group_id
 }
 
 output "node_security_group_id" {
-  description = "Security group ID attached to the EKS nodes"
-  value       = module.eks_cluster.node_security_group_id
+  value = module.eks_cluster.node_security_group_id
 }
 
 output "cluster_iam_role_arn" {
-  description = "IAM role ARN of the EKS cluster"
-  value       = module.eks_cluster.cluster_iam_role_arn
+  value = module.eks_cluster.cluster_iam_role_arn
 }
 
 output "node_iam_role_arn" {
-  description = "IAM role ARN of the EKS nodes"
-  value       = module.eks_cluster.node_iam_role_arn
+  value = module.eks_cluster.node_iam_role_arn
 }
 
 output "oidc_provider_arn" {
-  description = "OIDC provider ARN for IRSA"
-  value       = module.eks_cluster.oidc_provider_arn
+  value = module.eks_cluster.oidc_provider_arn
 }
 
 output "oidc_provider_url" {
-  description = "OIDC provider URL for IRSA"
-  value       = module.eks_cluster.oidc_provider_url
+  value = module.eks_cluster.oidc_provider_url
 }
 
 output "node_group_ids" {
-  description = "Map of EKS node group IDs"
-  value       = module.eks_cluster.node_group_ids
+  value = module.eks_cluster.node_group_ids
 }
 
 output "node_group_status" {
-  description = "Map of EKS node group statuses"
-  value       = module.eks_cluster.node_group_status
+  value = module.eks_cluster.node_group_status
 }
 
 output "kubeconfig" {
-  description = "Kubeconfig for cluster access"
-  value       = module.eks_cluster.kubeconfig
-  sensitive   = true
+  value     = module.eks_cluster.kubeconfig
+  sensitive = true
 }
 
 output "eks_addons" {
-  description = "Map of EKS addons and their versions"
-  value       = module.eks_cluster.eks_addons
+  value = module.eks_cluster.eks_addons
 }
 
 output "ebs_csi_driver_role_arn" {
-  description = "IAM role ARN for EBS CSI driver"
-  value       = module.eks_cluster.ebs_csi_driver_role_arn
+  value = module.eks_cluster.ebs_csi_driver_role_arn
 }
 
 output "cluster_autoscaler_role_arn" {
-  description = "IAM role ARN for Cluster Autoscaler"
-  value       = module.eks_cluster.cluster_autoscaler_role_arn
+  value = module.eks_cluster.cluster_autoscaler_role_arn
 }
